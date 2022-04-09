ee3f7d1010f4902b3d405d7e08e9f0a92bf1ff7d
ledungcobra <ledungcobra@gmail.com>
2022-04-10 01:01:19 +0700

    Zero edge is not allow

--- geometry/geometry.go
+++ geometry/geometry.go
@@ -1,5 +1,11 @@
 package geometry
 
-func CubeVolume(side float64) float64 {
-	return side * side * side
+import "errors"
+
+func CubeVolume(side float64) (float64, error) {
+	if side != 0 {
+		return side * side * side, nil
+	} else {
+		return 0, errors.New("side cannot be 0")
+	}
 }
